2cf089d2257b70a717c0af3dd2a953cf210ffaf4
artemdulina <artem.dulina@gmail.com>
2016-12-20 12:25:02 +0300

    added DAL layer

---
+++ ToDoList/DAL/Configurations/MapperDomainConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using DAL.DataTransferObject;
+
+namespace DAL.Configurations
+{
+    public static class MapperDomainConfiguration
+    {
+        public static IMapper MapperInstance { get; private set; }
+
+        static MapperDomainConfiguration()
+        {
+            Configure();
+        }
+
+        public static void Configure()
+        {
+            MapperInstance = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<ORM.Task, DalTask>().ReverseMap();
+            }).CreateMapper();
+        }
+    }
+}
---
+++ ToDoList/DAL/DependencyModules/DalModule.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+using DAL.Repository;
+using DAL.RepositoryImplementations;
+using Ninject.Modules;
+using ORM;
+
+namespace DAL.DependencyModules
+{
+    public class DalModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<DbContext>().To<ToDoContext>();
+            Bind<ITaskRepository>().To<TaskRepository>();
+        }
+    }
+}
